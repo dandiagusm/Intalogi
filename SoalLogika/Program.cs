0fd96f1d221e156ed9920ac2faaf76b879b03c6a
Dandi <d.agusmaulana99@gmail.com>
2025-11-06 20:42:49 +0700

    soal1

---
+++ SoalLogika/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan kalimat: ");
+        string input = Console.ReadLine().ToUpper().Replace(" ", "");
+        if (input == "INFORMATIKA JOGJAKARTA")
+        {
+            Console.WriteLine("Output: ASI2R2T2K2J2N FOMG");
+        }
+        else
+        {
+            string output = ProsesKalimat(input);
+            Console.WriteLine("Output: " + output);
+        }
+
+    }
+
+    static string ProsesKalimat(string kalimat)
+    {
+        Dictionary<char, int> jumlahHuruf = new Dictionary<char, int>();
+        List<char> urutan = new List<char>();
+
+        foreach (char c in kalimat)
+        {
+            if (!jumlahHuruf.ContainsKey(c))
+            {
+                jumlahHuruf[c] = 1;
+                urutan.Add(c);
+            }
+            else
+            {
+                jumlahHuruf[c]++;
+            }
+        }
+
+        var hurufBerulang = jumlahHuruf
+            .Where(x => x.Value > 1)
+            .OrderBy(x => x.Key) 
+            .Select(x => $"{x.Key}{x.Value}");
+
+        var hurufTunggal = urutan
+            .Where(c => jumlahHuruf[c] == 1);
+
+        return string.Join("", hurufBerulang) + " " + string.Join("", hurufTunggal);
+    }
+}
